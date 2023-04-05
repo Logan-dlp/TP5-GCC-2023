17296e37c7c5dfb9e88c6d3b5e18e2fd9aabbcd5
Nayboko <cedric.pluta@laposte.net>
2023-04-06 00:15:22 +0200

    :construction_worker: Add build bash script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+  echo "Usage: ./build.sh [-a|-m|-r]"
+  exit 1
+fi
+
+current_dir=$(pwd)
+cd "$(dirname "$0")"
+
+if [[ $1 = "-a" ]]; then
+  if [ -d "build" ]; then
+    echo "# Deleting existing build directory"
+    chmod -R 777 "./build"
+    rm -rf ./build
+  fi
+  echo "# Creating new build directory"
+  mkdir build
+  cd build
+  echo "# Running cmake"
+  cmake ..
+  echo "# Building the projet"
+  make
+elif [[ $1 = "-m" ]]; then
+  cd build
+  echo "# Building the projet"
+  make
+elif [[ $1 = "-r" ]]; then
+  cd build
+  echo "# Running the projet"
+  ./tp
+else
+  echo "Usage: ./build.sh [-a|-m|-r]"
+fi
+
+cd "$current_dir"
